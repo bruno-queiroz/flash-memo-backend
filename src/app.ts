5d9f0ea9e8b22b84977ff78ec3fcc0c28e4a26a2
bruno <brunoqueiroz612@gmail.com>
2023-05-05 15:24:43 -0300

    Create sign-up route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import { signUp } from "./controllers/SignUp";
 
+export const prisma = new PrismaClient();
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/sign-up", signUp);
 
 app.listen(3000, () => {
   console.log("running");
-});
-
-app.get("/", (req, res) => {
-  res.send("hello world!!!!!!!!!!");
 });
