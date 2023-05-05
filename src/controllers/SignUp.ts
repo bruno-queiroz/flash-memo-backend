08bded89f74142a45e98379c3343032fabdd956f
bruno <brunoqueiroz612@gmail.com>
2023-05-05 15:21:32 -0300

    Save new user in database

---
+++ src/controllers/SignUp.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import bcrypt from "bcrypt";
+import { prisma } from "../app";
+
+export const signUp = async (req: Request, res: Response) => {
+  const userSignUpData = req.body;
+
+  const hash = await bcrypt.hash(userSignUpData.password, 10);
+
+  await prisma.user.create({
+    data: {
+      name: userSignUpData.name,
+      password: hash,
+    },
+  });
+
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+
+  res.json({ msg: "user created" });
+};
