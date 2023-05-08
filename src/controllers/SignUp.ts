218e128c74901e94c855a89a067dbca2b4afd066
bruno <brunoqueiroz612@gmail.com>
2023-05-08 15:19:05 -0300

    Add custom prisma error on sign up

--- src/controllers/SignUp.ts
+++ src/controllers/SignUp.ts
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import cookie from "cookie";
 import bcrypt from "bcrypt";
 import { prisma } from "../app";
+import { Prisma } from "@prisma/client";
 
 export const signUp = async (req: Request, res: Response) => {
   const userSignUpData = req.body;
@@ -19,6 +20,17 @@ export const signUp = async (req: Request, res: Response) => {
 
     res.json({ isOk: true, msg: "User Created", data: null });
   } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === "P2002") {
+        res.json({
+          isOk: false,
+          msg: "Name must be unique",
+          data: null,
+        });
+      }
+      return;
+    }
+
     res.json({
       isOk: false,
       msg: "User not Created, something went wrong",
