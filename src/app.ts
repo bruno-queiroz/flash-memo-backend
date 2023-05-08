9a832779ec0c2df79b78aeb900f4bbbd6c1b54c7
bruno <brunoqueiroz612@gmail.com>
2023-05-08 15:58:40 -0300

    Add some corsOptions

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import express from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import { PrismaClient } from "@prisma/client";
 import { signUp } from "./controllers/SignUp";
 
 export const prisma = new PrismaClient();
 const app = express();
 
+const allowedUrls = ["http://localhost:5173"];
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (allowedUrls.indexOf(origin || "") !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.post("/sign-up", signUp);
 
