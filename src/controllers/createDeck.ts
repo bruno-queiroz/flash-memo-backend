64dd990b20f7359ea020c942eb6b3097baad6aa3
bruno <brunoqueiroz612@gmail.com>
2023-05-10 07:33:11 -0300

    Create createDeck controller

---
+++ src/controllers/createDeck.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { prisma } from "../app";
+import { Prisma } from "@prisma/client";
+
+interface DeckBodyRequest {
+  deckName: string;
+  password: string;
+  userName: string;
+  userId: string;
+}
+
+export const createDeck = async (req: Request, res: Response) => {
+  const userNewDeckData: DeckBodyRequest = req.body;
+
+  try {
+    await prisma.deck.create({
+      data: { name: userNewDeckData.deckName, userId: userNewDeckData.userId },
+    });
+
+    res.json({ isOk: true, msg: "Deck Created Successfully", data: null });
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === "P2002") {
+        res.json({
+          isOk: false,
+          msg: "You already have a deck with this name",
+          data: null,
+        });
+      } else {
+        res.json({ isOk: false, msg: "Something went wrong", data: null });
+      }
+    }
+  }
+};
