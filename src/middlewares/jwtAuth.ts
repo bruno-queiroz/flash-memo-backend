a37a58edc62b2e594617aa54871634827872d5a4
bruno <brunoqueiroz612@gmail.com>
2023-05-10 07:35:25 -0300

    Create CustomJwtPayload types to decoded value

--- src/middlewares/jwtAuth.ts
+++ src/middlewares/jwtAuth.ts
@@ -1,10 +1,23 @@
 import { Response, Request, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+interface CustomPayloadData {
+  userName?: string;
+  userId?: string;
+}
+
+type CustomJwtPayload = string | (jwt.JwtPayload & CustomPayloadData);
+
 export const jwtAuth = (req: Request, res: Response, next: NextFunction) => {
   const jwtToken = req.cookies["jwt-token"];
   try {
-    var decoded = jwt.verify(jwtToken, process.env.JWT_SECRET || "");
+    const decoded: CustomJwtPayload = jwt.verify(
+      jwtToken,
+      process.env.JWT_SECRET || ""
+    );
+    req.body.userName = (decoded as CustomPayloadData)?.userName;
+    req.body.userId = (decoded as CustomPayloadData)?.userId;
+
     next();
   } catch (err) {
     res.json({ isOk: false, msg: "jwt expired", data: null });
