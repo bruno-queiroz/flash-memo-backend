a511a024624fec8f2be6a63991871c1ab8507979
bruno <brunoqueiroz612@gmail.com>
2023-05-15 16:11:11 -0300

    Include card status on every deck

--- src/controllers/getDecks.ts
+++ src/controllers/getDecks.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { prisma } from "../app";
 import { Prisma } from "@prisma/client";
+import { getCardsExpired } from "../utils/getCardsExpired";
+import { countCardAmountGroups } from "../utils/countCardAmountGroups";
 
-interface BodyRequest {
+export interface BodyRequest {
   userName: string;
   userId: string;
 }
@@ -14,10 +16,23 @@ export const getDecks = async (req: Request, res: Response) => {
     const decks = await prisma.user.findUnique({
       where: { id: userData.userId },
       select: {
-        deck: true,
+        deck: {
+          include: {
+            cards: true,
+          },
+        },
       },
     });
-    res.json({ isOk: true, msg: "Queried Successfully", data: decks });
+
+    const decksWithStatus = decks?.deck?.map((deck) => ({
+      ...deck,
+      cards: countCardAmountGroups(deck?.cards?.filter(getCardsExpired)),
+    }));
+    res.json({
+      isOk: true,
+      msg: "Queried Successfully",
+      data: decksWithStatus,
+    });
   } catch (err) {
     res.json({ isOk: false, msg: "Something went wrong", data: null });
   }
