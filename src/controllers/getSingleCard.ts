b1aa96cd8b3572a796186bafe513abba8067af79
bruno <brunoqueiroz612@gmail.com>
2023-05-26 07:41:33 -0300

    Create getSingleCard controller

---
+++ src/controllers/getSingleCard.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { prisma } from "../app";
+
+export const getSingleCard = async (req: Request, res: Response) => {
+  const { cardId } = req.params;
+
+  try {
+    const card = await prisma.card.findUnique({
+      where: { id: cardId },
+    });
+
+    res.json({ isOk: true, msg: "Card found", data: card });
+  } catch (errr) {
+    res.json({ isOk: false, msg: "Card not found", data: null });
+  }
+};
