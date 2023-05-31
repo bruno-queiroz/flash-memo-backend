49e306055f199dce84a0a45c91ec8175f9d4ee6e
bruno <brunoqueiroz612@gmail.com>
2023-05-31 16:37:08 -0300

    Create deleteDeck controller

---
+++ src/controllers/deleteDeck.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { prisma } from "../app";
+
+export const deleteDeck = async (req: Request, res: Response) => {
+  const { deckId } = req.params;
+
+  try {
+    await prisma.card.deleteMany({
+      where: {
+        deckId,
+      },
+    });
+
+    await prisma.deck.delete({
+      where: {
+        id: deckId,
+      },
+    });
+
+    res.json({ isOk: true, msg: "Deck deleted", data: null });
+  } catch (err) {
+    res.json({ isOk: false, msg: "Failed to delete deck", data: null });
+  }
+};
