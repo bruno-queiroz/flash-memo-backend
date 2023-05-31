05e46f7dc3d9f6ed7c8d6c4b2c6573f13309fb92
bruno <brunoqueiroz612@gmail.com>
2023-05-31 15:37:17 -0300

    Create patchCardContent controller

---
+++ src/controllers/patchCardContent.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { prisma } from "../app";
+import { Prisma } from "@prisma/client";
+
+interface BodyRequest {
+  front: string;
+  back: string;
+  userName: string;
+  userId: string;
+}
+
+export const patchCardContent = async (req: Request, res: Response) => {
+  const { cardId } = req.params;
+  const { front, back }: BodyRequest = req.body;
+
+  const newCardContent = { front, back };
+  try {
+    await prisma.card.update({
+      where: { id: cardId },
+      data: newCardContent,
+    });
+
+    res.json({ isOk: true, msg: "Card edited", data: null });
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === "P2002") {
+        res.json({
+          isOk: false,
+          msg: "You already have this card",
+          data: null,
+        });
+      }
+      return;
+    }
+    res.json({
+      isOk: false,
+      msg: "Something went wrong editting the card",
+      data: null,
+    });
+  }
+};
