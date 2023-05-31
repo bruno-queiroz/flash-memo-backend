2b74c47f687dddcab6ad0dadca3a5ce6cfef48b3
bruno <brunoqueiroz612@gmail.com>
2023-05-31 15:39:21 -0300

    Filter expired cards

--- src/controllers/studyDeck.ts
+++ src/controllers/studyDeck.ts
@@ -1,12 +1,8 @@
 import { Request, Response } from "express";
 import { prisma } from "../app";
-import { Prisma } from "@prisma/client";
 import { getCardsExpired } from "../utils/getCardsExpired";
-import { countCardAmountGroups } from "../utils/countCardAmountGroups";
-import { BodyRequest } from "./getDecks";
 
 export const studyDeck = async (req: Request, res: Response) => {
-  const userData: BodyRequest = req.body;
   const { deckName } = req.params;
   try {
     const deck = await prisma.deck.findUnique({
@@ -15,8 +11,8 @@ export const studyDeck = async (req: Request, res: Response) => {
         cards: true,
       },
     });
-
-    res.json({ isOk: true, msg: "Cards Found", data: deck });
+    const expiredCards = deck?.cards.filter(getCardsExpired);
+    res.json({ isOk: true, msg: "Cards Found", data: expiredCards });
   } catch (err) {
     console.log(err);
     res.json({ isOk: false, msg: "Something went wrong", data: null });
