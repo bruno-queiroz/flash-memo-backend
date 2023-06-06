49e6c1f26b77053ab4d54ceaaf19fdc0a57c2f50
bruno <brunoqueiroz612@gmail.com>
2023-06-06 15:47:09 -0300

    Rearrange routes order and Add getLogOut route

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import { signIn } from "./controllers/signIn";
 import cookieParser from "cookie-parser";
 import { jwtAuth } from "./middlewares/jwtAuth";
 import dotenv from "dotenv";
-import { createDeck } from "./controllers/createDeck";
+import { postDeck } from "./controllers/postDeck";
 import { getDecks } from "./controllers/getDecks";
 import { createCard } from "./controllers/createCard";
 import { studyDeck } from "./controllers/studyDeck";
@@ -16,6 +16,7 @@ import { getSingleCard } from "./controllers/getSingleCard";
 import { patchCardContent } from "./controllers/patchCardContent";
 import { deleteDeck } from "./controllers/deleteDeck";
 import { deleteCard } from "./controllers/deleteCard";
+import { getLogOut } from "./controllers/getLogOut";
 
 export const prisma = new PrismaClient();
 const app = express();
@@ -39,14 +40,18 @@ dotenv.config();
 
 app.post("/sign-up", signUp);
 app.post("/sign-in", signIn);
-app.post("/create-deck", jwtAuth, createDeck);
+app.post("/create-deck", jwtAuth, postDeck);
 app.post("/create-card", jwtAuth, createCard);
+
 app.get("/get-decks", jwtAuth, getDecks);
 app.get("/study-deck/:deckName", jwtAuth, studyDeck);
-app.patch("/patch-card-dates/:cardId", jwtAuth, patchCardDates);
-app.patch("/patch-card-content/:cardId", jwtAuth, patchCardContent);
 app.get("/search-cards/:deckId/:cardQuery", jwtAuth, searchCard);
 app.get("/get-single-card/:cardId", jwtAuth, getSingleCard);
+app.get("/get-log-out", getLogOut);
+
+app.patch("/patch-card-dates/:cardId", jwtAuth, patchCardDates);
+app.patch("/patch-card-content/:cardId", jwtAuth, patchCardContent);
+
 app.delete("/delete-deck/:deckId", jwtAuth, deleteDeck);
 app.delete("/delete-card/:cardId", jwtAuth, deleteCard);
 
