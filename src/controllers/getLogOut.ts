0cc473ce8e59f6b0eeff0c9c3b808b3c74c92e2e
bruno <brunoqueiroz612@gmail.com>
2023-06-09 16:15:03 -0300

    Clear is-user-logged cookie when user logs out

--- src/controllers/getLogOut.ts
+++ src/controllers/getLogOut.ts
@@ -2,5 +2,6 @@ import { Request, Response } from "express";
 
 export const getLogOut = async (req: Request, res: Response) => {
   res.clearCookie("jwt-token");
+  res.clearCookie("is-user-logged");
   res.json({ isOk: true, msg: "User Logged out", data: null });
 };
