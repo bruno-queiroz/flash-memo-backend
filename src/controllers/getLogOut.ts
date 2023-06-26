8f3037d364f7e4e0fb2482a5da246ce576e17650
bruno <brunoqueiroz612@gmail.com>
2023-06-26 08:34:10 -0300

    Clear cookies properly

--- src/controllers/getLogOut.ts
+++ src/controllers/getLogOut.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
 
 export const getLogOut = async (req: Request, res: Response) => {
-  res.clearCookie("jwt-token");
-  res.clearCookie("is-user-logged");
+  res.clearCookie("jwt-token", {
+    httpOnly: true,
+    maxAge: 60 * 60 * 24 * 7, // 1 week
+    sameSite: "none",
+    secure: true,
+  });
+
+  res.clearCookie("is-user-logged", {
+    maxAge: 365 * 24 * 60 * 60 * 1000, // 1 year
+    sameSite: "none",
+    secure: true,
+  });
+
   res.json({ isOk: true, msg: "User Logged out", data: null });
 };
