1703d3bbd02cfdd14df5b1cf5cd8149afc07f651
bruno <brunoqueiroz612@gmail.com>
2023-06-28 08:52:01 -0300

    Add new url to cors

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ export const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedUrls = ["http://localhost:5173", "http://localhost:3000"];
+const allowedUrls = ["http://localhost:5173", "https://flash-memo.vercel.app"];
 const corsOptions: CorsOptions = {
   origin: function (origin, callback) {
     if (allowedUrls.indexOf(origin || "") !== -1) {
