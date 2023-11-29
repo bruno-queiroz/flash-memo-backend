1ab5095576f02f62aac6870606b01a333d99bcf3
bruno <brunoqueiroz612@gmail.com>
2023-11-29 10:39:34 -0300

    Create POST to /deck should an error response test

--- __tests__/createCard.test.ts
+++ __tests__/createCard.test.ts
@@ -3,6 +3,7 @@ import { createApp } from "../src/app";
 import { allowedUrl } from "./mocks/allowedUrl";
 import { generateJwtToken } from "./mocks/generateJwtToken";
 import { prismaMock } from "./mocks/singleton";
+import { Prisma } from "@prisma/client";
 
 const app = createApp();
 const jwtToken = generateJwtToken();
@@ -21,9 +22,22 @@ describe("Create cards", () => {
       .set("Origin", allowedUrl)
       .send(card);
 
-    console.log("checking body", response.body);
     expect(cardCreatedMocked.mock.calls).toHaveLength(1);
     expect(response.body?.data).toMatchObject(card);
     expect(response.status).toBe(201);
+  });
+  it("POST to /deck should an error response", async () => {
+    const cardCreatedMocked = prismaMock.card.create.mockRejectedValue(
+      Prisma.PrismaClientKnownRequestError
+    );
+    const response = await request(app)
+      .post("/card")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl)
+      .send();
+
+    expect(cardCreatedMocked.mock.calls).toHaveLength(1);
+    expect(response.body?.isOk).toBe(false);
+    expect(response.status).toBe(500);
   });
 });
