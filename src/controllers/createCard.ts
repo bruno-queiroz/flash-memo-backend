fbcc0eab296f7fd72c8f79135326657c3786a09e
bruno <brunoqueiroz612@gmail.com>
2023-11-29 11:14:42 -0300

    Refactor catch error response

--- src/controllers/createCard.ts
+++ src/controllers/createCard.ts
@@ -23,18 +23,21 @@ export const createCard = async (req: Request, res: Response) => {
     res.status(201).json({ isOk: true, msg: "Card Created", data: card });
   } catch (err) {
     console.log(err);
-    if (err instanceof Prisma.PrismaClientKnownRequestError) {
-      if (err.code === "P2002") {
-        res.status(400).json({
-          isOk: false,
-          msg: "You already have this card",
-          data: null,
-        });
-      }
-    } else {
-      res
-        .status(500)
-        .json({ isOk: false, msg: "Something went wrong", data: null });
+
+    if (
+      err instanceof Prisma.PrismaClientKnownRequestError &&
+      err.code === "P2002"
+    ) {
+      res.status(400).json({
+        isOk: false,
+        msg: "You already have this card",
+        data: null,
+      });
+      return;
     }
+
+    res
+      .status(500)
+      .json({ isOk: false, msg: "Something went wrong", data: null });
   }
 };
