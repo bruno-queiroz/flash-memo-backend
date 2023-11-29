b48c3f7514f17da091285c629891c4ec8b342713
bruno <brunoqueiroz612@gmail.com>
2023-11-29 11:14:18 -0300

    Create POST to /deck with same name should fail test

--- __tests__/createCard.test.ts
+++ __tests__/createCard.test.ts
@@ -27,9 +27,7 @@ describe("Create cards", () => {
     expect(response.status).toBe(201);
   });
   it("POST to /deck should return an error response", async () => {
-    const cardCreatedMocked = prismaMock.card.create.mockRejectedValue(
-      Prisma.PrismaClientKnownRequestError
-    );
+    const cardCreatedMocked = prismaMock.card.create.mockRejectedValue("");
     const response = await request(app)
       .post("/card")
       .set("Cookie", ["jwt-token=" + jwtToken])
@@ -48,5 +46,24 @@ describe("Create cards", () => {
 
     expect(response.body?.isOk).toBe(false);
     expect(response.status).toBe(401);
+  });
+  it("POST to /deck with same name should fail", async () => {
+    const cardCreatedMocked = prismaMock.card.create.mockRejectedValue(
+      new Prisma.PrismaClientKnownRequestError("error", {
+        code: "P2002",
+        clientVersion: "1",
+        batchRequestIdx: 1,
+        meta: { test: 1 },
+      })
+    );
+    const response = await request(app)
+      .post("/card")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl)
+      .send();
+
+    expect(cardCreatedMocked.mock.calls).toHaveLength(1);
+    expect(response.body?.isOk).toBe(false);
+    expect(response.status).toBe(400);
   });
 });
