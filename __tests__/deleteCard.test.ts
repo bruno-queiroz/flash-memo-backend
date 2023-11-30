fc1f8d20a6ad64be878ae472c0dee1c258299274
bruno <brunoqueiroz612@gmail.com>
2023-11-30 10:45:52 -0300

    Create DELETE to /card/:cardId happy path test

---
+++ __tests__/deleteCard.test.ts
@@ -0,0 +1,24 @@
+import request from "supertest";
+import { createApp } from "../src/app";
+import { allowedUrl } from "./mocks/allowedUrl";
+import { generateJwtToken } from "./mocks/generateJwtToken";
+import { prismaMock } from "./mocks/singleton";
+
+const app = createApp();
+const jwtToken = generateJwtToken();
+
+describe("Testing deleteCard controller", () => {
+  it("DELETE to /card/:cardId should be successful", async () => {
+    const mocked = prismaMock.card.delete.mockResolvedValue({} as any);
+
+    const response = await request(app)
+      .delete("/card/123")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl)
+      .send();
+
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.isOk).toBe(true);
+    expect(response.status).toBe(200);
+  });
+});
