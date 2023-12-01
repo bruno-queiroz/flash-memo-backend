05bcd9f568830789f7321fe6b1cbcce885f521e4
bruno <brunoqueiroz612@gmail.com>
2023-12-01 10:41:57 -0300

    Create GET to /deck should be successful test

---
+++ __tests__/getDecks.test.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+import { createApp } from "../src/app";
+import { allowedUrl } from "./mocks/allowedUrl";
+import { generateJwtToken } from "./mocks/generateJwtToken";
+import { prismaMock } from "./mocks/singleton";
+import { deckWithStatus } from "./mocks/deckWithStatus";
+import { decksAndCardsReadyToReview } from "./mocks/decksAndCardsReadyToReview";
+
+const app = createApp();
+const jwtToken = generateJwtToken();
+
+describe("Testing getDecks controller", () => {
+  it("GET to /deck should be successful", async () => {
+    const mocked = prismaMock.user.findUnique.mockResolvedValue(
+      decksAndCardsReadyToReview as any
+    );
+
+    const response = await request(app)
+      .get("/deck")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl);
+
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.data).toMatchObject(deckWithStatus);
+    expect(response.status).toBe(200);
+  });
+});
