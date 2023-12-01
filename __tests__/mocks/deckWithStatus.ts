b830750f170d4e987716ebef0413932999b1fcf8
bruno <brunoqueiroz612@gmail.com>
2023-12-01 10:42:48 -0300

    Create deckWithStatus mock

---
+++ __tests__/mocks/deckWithStatus.ts
@@ -0,0 +1,13 @@
+export const deckWithStatus = [
+  {
+    id: "clmox1g210003v36tsepvlcib",
+    name: "First deck",
+    createdAt: "2023-09-18T13:22:20.425Z",
+    userId: "clmox1bex0000v36tfbkzt4wo",
+    cards: {
+      resetedCards: 0,
+      newCards: 0,
+      reviewCards: 1,
+    },
+  },
+];
