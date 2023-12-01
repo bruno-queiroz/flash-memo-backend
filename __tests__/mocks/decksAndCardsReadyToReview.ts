4059e4f6be953cc22ae672fb83bb97e99573f906
bruno <brunoqueiroz612@gmail.com>
2023-12-01 10:42:37 -0300

    Create decksAndCardsReadyToReview mock

---
+++ __tests__/mocks/decksAndCardsReadyToReview.ts
@@ -0,0 +1,22 @@
+export const decksAndCardsReadyToReview = {
+  deck: [
+    {
+      id: "clmox1g210003v36tsepvlcib",
+      name: "First deck",
+      createdAt: "2023-09-18T13:22:20.425Z",
+      userId: "clmox1bex0000v36tfbkzt4wo",
+      cards: [
+        {
+          id: "clmoxir610001v3a7oc1ee7zq",
+          front: "ui",
+          back: "ui",
+          createdAt: "2023-09-18T13:35:47.976Z",
+          reviewAt: "2023-11-20T16:02:44.451Z",
+          reviewAwaitTime: 7200000,
+          deckId: "clmox1g210003v36tsepvlcib",
+          wasCardReseted: false,
+        },
+      ],
+    },
+  ],
+};
