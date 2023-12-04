a5621ceaea938d0091e56a32ca3ad1a2fe21c517
bruno <brunoqueiroz612@gmail.com>
2023-12-04 11:24:16 -0300

    Refactor catch part of the controller

--- src/controllers/patchCardContent.ts
+++ src/controllers/patchCardContent.ts
@@ -22,16 +22,20 @@ export const patchCardContent = async (req: Request, res: Response) => {
 
     res.status(200).json({ isOk: true, msg: "Card edited", data: cardEdited });
   } catch (err) {
-    if (err instanceof Prisma.PrismaClientKnownRequestError) {
-      if (err.code === "P2002") {
-        res.status(400).json({
-          isOk: false,
-          msg: "You already have this card",
-          data: null,
-        });
-      }
+    console.error("Error editing card", err);
+
+    if (
+      err instanceof Prisma.PrismaClientKnownRequestError &&
+      err.code === "P2002"
+    ) {
+      res.status(400).json({
+        isOk: false,
+        msg: "You already have this card",
+        data: null,
+      });
       return;
     }
+
     res.status(500).json({
       isOk: false,
       msg: "Something went wrong editing the card",
