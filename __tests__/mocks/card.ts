c6152972f885e207ea9ee833bd93b2a91dd9c556
bruno <brunoqueiroz612@gmail.com>
2023-12-04 10:58:53 -0300

    Create card mock data

---
+++ __tests__/mocks/card.ts
@@ -0,0 +1,10 @@
+export const card = {
+  id: "123",
+  front: "test",
+  back: "test",
+  createdAt: "2023-12-04T13:51:31.774Z",
+  deckId: "123",
+  reviewAt: "2023-12-04T13:51:31.774Z",
+  reviewAwaitTime: 123,
+  wasCardReseted: false,
+};
