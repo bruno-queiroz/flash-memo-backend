523dcb220df0b4e8129f51df666d2db8242adc0e
bruno <brunoqueiroz612@gmail.com>
2023-12-05 11:14:48 -0300

    Create unhappy path test

--- __tests__/patchCardDates.test.ts
+++ __tests__/patchCardDates.test.ts
@@ -20,4 +20,16 @@ describe("Testing patchCardDates controller", () => {
     expect(response.body?.isOk).toBe(true);
     expect(response.status).toBe(200);
   });
+  it("PATCH to /card-date/:cardId should fail and return fail response", async () => {
+    const mocked = prismaMock.card.update.mockRejectedValue("");
+
+    const response = await request(app)
+      .patch("/card-date/123")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl);
+
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.isOk).toBe(false);
+    expect(response.status).toBe(500);
+  });
 });
