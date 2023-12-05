097cdd7c4e4d2297754c3d39d35e9c8038190c8d
bruno <brunoqueiroz612@gmail.com>
2023-12-05 11:10:12 -0300

    Create happy path test

---
+++ __tests__/patchCardDates.test.ts
@@ -0,0 +1,23 @@
+import request from "supertest";
+import { createApp } from "../src/app";
+import { allowedUrl } from "./mocks/allowedUrl";
+import { generateJwtToken } from "./mocks/generateJwtToken";
+import { prismaMock } from "./mocks/singleton";
+
+const app = createApp();
+const jwtToken = generateJwtToken();
+
+describe("Testing patchCardDates controller", () => {
+  it("PATCH to /card-date/:cardId should be successful", async () => {
+    const mocked = prismaMock.card.update.mockResolvedValue("" as any);
+
+    const response = await request(app)
+      .patch("/card-date/123")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl);
+
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.isOk).toBe(true);
+    expect(response.status).toBe(200);
+  });
+});
