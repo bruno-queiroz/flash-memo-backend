2bc96c7131001ebac6b89ec5f0b25a62d3ba4521
bruno <brunoqueiroz612@gmail.com>
2023-12-06 11:00:30 -0300

    Add status code

--- src/controllers/postDeck.ts
+++ src/controllers/postDeck.ts
@@ -17,18 +17,22 @@ export const postDeck = async (req: Request, res: Response) => {
       data: { name: userNewDeckData.deckName, userId: userNewDeckData.userId },
     });
 
-    res.json({ isOk: true, msg: "Deck Created Successfully", data: null });
+    res
+      .status(200)
+      .json({ isOk: true, msg: "Deck Created Successfully", data: null });
   } catch (err) {
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
       if (err.code === "P2002") {
-        res.json({
+        res.status(400).json({
           isOk: false,
           msg: "You already have a deck with this name",
           data: null,
         });
       }
     } else {
-      res.json({ isOk: false, msg: "Something went wrong", data: null });
+      res
+        .status(500)
+        .json({ isOk: false, msg: "Something went wrong", data: null });
     }
   }
 };
