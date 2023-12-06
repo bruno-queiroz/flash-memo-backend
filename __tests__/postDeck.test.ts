a55f1e4ce5b662214a842cfc6e8ca4197d585c19
bruno <brunoqueiroz612@gmail.com>
2023-12-06 11:28:34 -0300

    Create unhappy path test

--- __tests__/postDeck.test.ts
+++ __tests__/postDeck.test.ts
@@ -23,4 +23,16 @@ describe("Testing postDeck controller", () => {
     expect(response.body?.isOk).toBe(true);
     expect(response.status).toBe(200);
   });
+  it("POST to /deck should fail and return fail response", async () => {
+    const mocked = prismaMock.deck.create.mockRejectedValue("");
+
+    const response = await request(app)
+      .post("/deck")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl);
+
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.isOk).toBe(false);
+    expect(response.status).toBe(500);
+  });
 });
