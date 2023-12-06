dc110c4df2c89d2462209cb9b0ed48941db26c45
bruno <brunoqueiroz612@gmail.com>
2023-12-06 10:51:28 -0300

    Create unhappy path test

--- __tests__/patchRenameDeck.test.ts
+++ __tests__/patchRenameDeck.test.ts
@@ -20,4 +20,16 @@ describe("Testing patchRenameDeck controller", () => {
     expect(response.body?.isOk).toBe(true);
     expect(response.status).toBe(200);
   });
+  it("PATCH to /deck/:deckId should fail and return fail response", async () => {
+    const mocked = prismaMock.deck.update.mockRejectedValue("" as any);
+
+    const response = await request(app)
+      .patch("/deck/123")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl);
+
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.isOk).toBe(false);
+    expect(response.status).toBe(500);
+  });
 });
