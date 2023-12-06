f9db39e8edbe00bc27c6c4f4233b85895c0df587
bruno <brunoqueiroz612@gmail.com>
2023-12-06 10:56:16 -0300

    Create same deck name unhappy path test

--- __tests__/patchRenameDeck.test.ts
+++ __tests__/patchRenameDeck.test.ts
@@ -3,6 +3,7 @@ import { createApp } from "../src/app";
 import { allowedUrl } from "./mocks/allowedUrl";
 import { generateJwtToken } from "./mocks/generateJwtToken";
 import { prismaMock } from "./mocks/singleton";
+import { Prisma } from "@prisma/client";
 
 const app = createApp();
 const jwtToken = generateJwtToken();
@@ -31,5 +32,24 @@ describe("Testing patchRenameDeck controller", () => {
     expect(mocked.mock.calls).toHaveLength(1);
     expect(response.body?.isOk).toBe(false);
     expect(response.status).toBe(500);
+  });
+  it("PATCH to /deck/:deckId with same deck name should fail", async () => {
+    const mocked = prismaMock.deck.update.mockRejectedValue(
+      new Prisma.PrismaClientKnownRequestError("error", {
+        code: "P2002",
+        clientVersion: "1",
+        batchRequestIdx: 1,
+        meta: { test: 1 },
+      })
+    );
+    const response = await request(app)
+      .patch("/deck/123")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl)
+      .send();
+
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.isOk).toBe(false);
+    expect(response.status).toBe(400);
   });
 });
