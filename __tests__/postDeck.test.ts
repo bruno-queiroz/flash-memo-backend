000b08bd4a5164de66f356edd7175c2ba16a0035
bruno <brunoqueiroz612@gmail.com>
2023-12-07 10:45:06 -0300

    Create test for same deck name unhappy path

--- __tests__/postDeck.test.ts
+++ __tests__/postDeck.test.ts
@@ -35,4 +35,22 @@ describe("Testing postDeck controller", () => {
     expect(response.body?.isOk).toBe(false);
     expect(response.status).toBe(500);
   });
+  it("POST to /deck with same deck name should fail", async () => {
+    const mocked = prismaMock.deck.create.mockRejectedValue(
+      new Prisma.PrismaClientKnownRequestError("error", {
+        code: "P2002",
+        clientVersion: "1",
+        batchRequestIdx: 1,
+        meta: { test: 1 },
+      })
+    );
+    const response = await request(app)
+      .post("/deck")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl);
+
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.isOk).toBe(false);
+    expect(response.status).toBe(400);
+  });
 });
