990aecbd3eba71418d276e21514ab7ae3b6c150d
bruno <brunoqueiroz612@gmail.com>
2023-12-07 11:03:19 -0300

    Create unhappy path test

--- __tests__/searchCard.test.ts
+++ __tests__/searchCard.test.ts
@@ -3,13 +3,12 @@ import { createApp } from "../src/app";
 import { allowedUrl } from "./mocks/allowedUrl";
 import { generateJwtToken } from "./mocks/generateJwtToken";
 import { prismaMock } from "./mocks/singleton";
-import { deck } from "./mocks/deck";
 
 const app = createApp();
 const jwtToken = generateJwtToken();
 
 describe("Testing searchCard controller", () => {
-  it("GET to /card/:deckId/:cardQuery  should be successful", async () => {
+  it("GET to /card/:deckId/:cardQuery should be successful", async () => {
     const mocked = prismaMock.deck.findUnique.mockResolvedValue("" as any);
 
     const response = await request(app)
@@ -19,5 +18,17 @@ describe("Testing searchCard controller", () => {
 
     expect(mocked.mock.calls).toHaveLength(1);
     expect(response.status).toBe(200);
+  });
+  it("GET to /card/:deckId/:cardQuery should fail and return fail response", async () => {
+    const mocked = prismaMock.deck.findUnique.mockRejectedValue("");
+
+    const response = await request(app)
+      .get("/card/123/a")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl);
+
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.isOk).toBe(false);
+    expect(response.status).toBe(400);
   });
 });
