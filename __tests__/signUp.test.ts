2a31db635c09193b0eae68c83984695312958831
bruno <brunoqueiroz612@gmail.com>
2023-12-11 11:20:38 -0300

    Create happy path test

---
+++ __tests__/signUp.test.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { createApp } from "../src/app";
+import { jest } from "@jest/globals";
+import { allowedUrl } from "./mocks/allowedUrl";
+import { prismaMock } from "./mocks/singleton";
+import { user } from "./mocks/user";
+import { mocked as jestMocked } from "jest-mock";
+import bcrypt from "bcrypt";
+
+jest.mock("bcrypt");
+
+const app = createApp();
+
+describe("Testing signUp controller", () => {
+  it("POST to /sign-up should be successful", async () => {
+    const mocked = prismaMock.user.create.mockResolvedValue(user as any);
+
+    const bcryptMocked = jestMocked(bcrypt).hash.mockResolvedValue("" as never);
+
+    const response = await request(app)
+      .post("/sign-up")
+      .set("Origin", allowedUrl)
+      .send({ name: "jubi", password: "123" });
+
+    expect(bcryptMocked.mock.calls).toHaveLength(1);
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.data).toMatchObject({ name: user.name, id: user.id });
+    expect(response.body?.isOk).toBe(true);
+    expect(response.status).toBe(200);
+  });
+});
