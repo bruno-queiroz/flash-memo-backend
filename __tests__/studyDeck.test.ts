21736d3e49e23173c9dfaed86af32028393f8e23
bruno <brunoqueiroz612@gmail.com>
2023-12-12 11:08:49 -0300

    Create happy path test

---
+++ __tests__/studyDeck.test.ts
@@ -0,0 +1,26 @@
+import request from "supertest";
+import { createApp } from "../src/app";
+import { allowedUrl } from "./mocks/allowedUrl";
+import { generateJwtToken } from "./mocks/generateJwtToken";
+import { prismaMock } from "./mocks/singleton";
+import { card } from "./mocks/card";
+
+const app = createApp();
+const jwtToken = generateJwtToken();
+
+describe("Testing studyDeck controller", () => {
+  it("GET to /deck/:deckName should be successful", async () => {
+    const mocked = prismaMock.deck.findFirstOrThrow.mockResolvedValue({
+      cards: card,
+    } as any);
+
+    const response = await request(app)
+      .get("/deck/deck")
+      .set("Cookie", ["jwt-token=" + jwtToken])
+      .set("Origin", allowedUrl);
+
+    expect(mocked.mock.calls).toHaveLength(1);
+    expect(response.body?.data).toMatchObject(card);
+    expect(response.status).toBe(200);
+  });
+});
